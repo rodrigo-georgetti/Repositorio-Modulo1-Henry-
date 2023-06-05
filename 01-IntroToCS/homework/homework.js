858ae0d6551ada41589f8a2bfec5ab45ff340e0f
RMGeorgetti <rodrigogeorgetti.cba@gmail.com>
2023-06-04 22:07:55 -0300

    homework 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,10 +1,26 @@
-'use strict';
+"use strict";
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+  var numeroBinario = num;
+  var numeroDecimal = 0;
+  for (let i = 0; i < numeroBinario.length; i++) {
+    numeroDecimal +=
+      numeroBinario[i] * Math.pow(2, numeroBinario.length - i - 1);
+  }
+  return numeroDecimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+  var numeroDecimal = num;
+  var numeroBinario = "";
+  while (numeroDecimal > 0) {
+    numeroBinario += numeroDecimal % 2;
+    numeroDecimal = Math.floor(numeroDecimal / 2);
+  }
+  return numeroBinario.split("").reverse().join("");
+}
 
 module.exports = {
-   BinarioADecimal,
-   DecimalABinario,
+  BinarioADecimal,
+  DecimalABinario,
 };
