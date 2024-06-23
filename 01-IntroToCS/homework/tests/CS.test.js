edeead9093f28d71c65139c5d924d6d5466aaf33
Rodrigo <rodrigogeorgetti.cba@gmail.com>
2024-06-22 21:43:34 -0300

    repaso de test

--- 01-IntroToCS/homework/tests/CS.test.js
+++ 01-IntroToCS/homework/tests/CS.test.js
@@ -6,20 +6,20 @@ const {
   DecimalABinario,
 } = require('../homework.js');
 
-describe('BinarioADecimal(num)', function() {
-  it('should return 2', function() {
+describe('BinarioADecimal(num)', function () {
+  it('should return 2', function () {
     expect(BinarioADecimal('10')).toBe(2);
   });
-  it('should return 7', function() {
+  it('should return 7', function () {
     expect(BinarioADecimal('111')).toBe(7);
   });
 });
 
-describe('DecimalABinario(num)', function() {
-  it('should return "100"', function() {
+describe('DecimalABinario(num)', function () {
+  it('should return "100"', function () {
     expect(DecimalABinario(4)).toBe('100');
   });
-  it('should return "111"', function() {
+  it('should return "111"', function () {
     expect(DecimalABinario(7)).toBe('111');
   });
 });
